792e94a9284c04274eeba888f38adc1c2bf6b91c
themooreyouknow1 <jacob.robert.moore@gmail.com>
2025-03-03 13:30:30 -0800

    Ticket 5 complete

--- src/main/java/com/example/task/management/system/controller/TaskController.java
+++ src/main/java/com/example/task/management/system/controller/TaskController.java
@@ -1,10 +1,38 @@
 package com.example.task.management.system.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.task.management.system.model.Task;
+import com.example.task.management.system.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
-    private TaskService
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+    @GetMapping("/{id}")
+    public Optional<Task> getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+    //this might need some help
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTaskById(id);
+    }
 }
